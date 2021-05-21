cfe6d8238cca1ef975856e7b133609e89f2628c1
Arihant <jarihant701@gmail.com>
2021-05-21 17:43:56 +0530

    Added dotenv package

--- server.js
+++ server.js
@@ -1,7 +1,10 @@
+require('dotenv').config();
+
 const express = require('express');
 const app = express();
 
 const PORT = 5000 || process.env.PORT;
+const DB_URI = process.env.DB;
 
 app.get('/', (req, res) => res.send('Hello World'));
 
