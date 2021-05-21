fb96b2f22397b04d97b4defea4ab74da0a021145
Arihant <jarihant701@gmail.com>
2021-05-21 18:13:38 +0530

    Connected DB

--- server.js
+++ server.js
@@ -2,9 +2,17 @@ require('dotenv').config();
 
 const express = require('express');
 const app = express();
+const mongoose = require('mongoose');
 
 const PORT = 5000 || process.env.PORT;
 const DB_URI = process.env.DB;
+
+// Connecting DB
+mongoose.connect(
+  DB_URI,
+  { useNewUrlParser: true, useUnifiedTopology: true },
+  () => console.log('Database connection successful')
+);
 
 app.get('/', (req, res) => res.send('Hello World'));
 
