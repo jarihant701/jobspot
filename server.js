7ae8bd8f2373eb822a3f677f6dea680519001db0
Arihant <jarihant701@gmail.com>
2021-05-21 17:35:26 +0530

    Project Started

---
+++ server.js
@@ -0,0 +1,8 @@
+const express = require('express');
+const app = express();
+
+const PORT = 5000 || process.env.PORT;
+
+app.get('/', (req, res) => res.send('Hello World'));
+
+app.listen(PORT, () => console.log(`Server started at PORT ${PORT}`));
